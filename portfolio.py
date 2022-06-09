1d123a59cb7b77bf9946dbe6de843738a67124ae
Alvaro Machuca <alvaromachucar@gmail.com>
2022-06-09 17:21:44 -0400

    Deleted try/except clause from purchase_stock method, added comments

--- portfolio.py
+++ portfolio.py
@@ -5,17 +5,20 @@ def parse_date(str_date):
     return datetime.strptime(str_date, '%d/%m/%Y')
 
 class Stock:
+    # track all instances of Stocks that have been created
     instances = []
 
     def __init__(self, price, add_date):
         self.price = price
         self.add_date = parse_date(add_date)
 
+        # add instance to class variable for tracking
         self.__class__.instances.append(self)
 
 
     @classmethod
     def price(cls, search_date):
+        #if there is an instance that matches in date, return price, else None
         for stock in cls.instances:
             if stock.add_date == parse_date(search_date):
                 return stock.price
@@ -55,12 +58,19 @@ class Portfolio:
     def purchase_stock(self, purchase_date, investment):
         price = Stock.price(purchase_date)
         
-        try:
+        # if price is None, then the stock with the specified date doesn't exist
+        if price is None:
+            print('No price information exists for date {}'.format(purchase_date))
+            return
+
+        # price is a number, we can proceed
+        else:
             if 0 < investment and 0 < price:
                 shares_bought = investment / price
                 self.total_shares += shares_bought 
                 self.total_investment += investment
 
+                # add current purchase to assets
                 self.assets.append({
                     'stock': Stock.get_stock(purchase_date),
                     'investment': investment,
@@ -70,13 +80,10 @@ class Portfolio:
             print('Stock purchase successful, {} shares @ {}'.format(shares_bought, price))
             return
 
-        except:
-            print('No price information exists for date {}'.format(purchase_date))
-            return
-
 
     def profit(self, start_date, end_date):
         
+        # check date range is valid
         if parse_date(end_date) <= parse_date(start_date):
             print('Invalid date range')
             return
@@ -92,14 +99,21 @@ class Portfolio:
             accumulated_shares = 0
             accumulated_investment = 0
 
+            # check inside assets all purchases that fall within the date range provided
             for transaction in self.assets:
                 if parse_date(start_date) <= transaction['stock'].add_date and transaction['stock'].add_date <= parse_date(end_date):
                     accumulated_investment += transaction['investment']
                     accumulated_shares += transaction['shares']
 
+            # compute statistics
             current_portfolio_value = accumulated_shares * latest_price
             profit = current_portfolio_value - accumulated_investment
-            print('Portfolio profit is {}'.format(profit))
+            
+            roi = (profit / accumulated_investment) * 100
+            
+            print('Total amount invested: {}'.format(self.total_investment))
+            print('Number of shares currently held: {0:.2f}'.format(self.total_shares))
+            print('Portfolio profit: {}'.format(profit))
+            print('Portfolio return: {0:.2f}%'.format(roi))
 
-            roi = profit / accumulated_investment
-            print('Portfolio return is {}'.format(roi))
+            return
